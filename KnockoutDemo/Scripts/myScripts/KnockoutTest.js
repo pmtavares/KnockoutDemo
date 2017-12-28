ec8a94dec9925c990e1f095912236e22cbf93d86
B00100855 <pedro.tavares@student.itb.ie>
2017-12-28 21:34:55 +0000

    Select button and delete

--- KnockoutDemo/Scripts/myScripts/KnockoutTest.js
+++ KnockoutDemo/Scripts/myScripts/KnockoutTest.js
@@ -12,25 +12,32 @@ ko.applyBindings(new NameViewModel("pedro", "Tavares"))
 Part 2: Account
 */
 //Model Object
-function Account(id, name, balance) {
+function Account(id, name, balance, deposits) {
     this.Id = id;
     this.Name = name;
-    this.Balance = balance;
+    this.Balance = ko.observable(balance);
+    this.Deposits = deposits;
 }
 var acc = new Account(1, "A1", 1000);
 //View Model
 function AccountViewModel(acc)
 {
     var self = this;
+
+    self.SelectedAccount = ko.observable();
     this.Id = acc.Id;
     this.Name = acc.Name;
     this.Balance = ko.observable(acc.Balance);
-    var a1 = new Account(1, "A1", 1000);
-    var a2 = new Account(2, "A2", 2000);
-    var a3 = new Account(3, "A3", 3000);
+    this.Deposits = acc.deposits;
+
+    var a1 = new Account(1, "A1", 1000, [1000,1500,2200]);
+    var a2 = new Account(2, "A2", 2000, [900, 800, 500]);
+    var a3 = new Account(3, "A3", 3000, [5600, 1500]);
     var a4 = new Account(3, "A3", 3000);
     self.Accounts = ko.observableArray([a1, a2, a3])
+
     self.SelectedAccountIndex = ko.observable(0);
+
     this.TotalBalance = ko.computed(function () {
         var total = 0;
         for (var i = 0; i < self.Accounts().length; i++) {
@@ -43,7 +50,29 @@ function AccountViewModel(acc)
     self.showHideSection = ko.observable(true);
     self.Phone = { home: 123456, office: 654321 };
     self.Colors = ko.observable(["red", "blue", "green"]);
-    self.Accounts2 = ko.observableArray([a1, a2,a4])
+    self.Accounts2 = ko.observableArray([a1, a2, a4])
+
+    self.Count = ko.computed(function () {
+        return self.Accounts2().length;
+
+    });
+
+    self.SelecteAccount = function (account) {
+        self.SelectedAccount(account);
+        alert("selected");
+    }
+
+    self.AmountToDeposit = ko.observable(0);
+    self.Deposit = function () {
+        
+        self.SelectedAccount().Balance(self.SelectedAccount().Balance() + Number(self.AmountToDeposit()));
+        alert("Deposited");
+    }
+
+    self.DeleteAccount = function (account) {
+        self.Accounts2.remove(account);
+        alert("Deleted");
+    }
 }
 
 var accViewModel = new AccountViewModel(acc);
